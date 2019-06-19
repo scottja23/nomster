8fdea03f65e918db84941de925e2f98376eac287
Jackson Scott <scottja23@gmail.com>
2019-06-19 17:26:21 +0000

    added show.html.erb and linked with controller

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,7 +2,7 @@ class PlacesController < ApplicationController
 before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    @places = Place.all
+    @places = Place.all.paginate(page: params[:page], per_page: 5)
   end
 
   def new
@@ -12,6 +12,10 @@ before_action :authenticate_user!, only: [:new, :create]
   def create
     current_user.places.create(place_params)
     redirect_to root_path
+  end
+
+  def show
+    @place = Place.find(params[:id])
   end
 
   private
