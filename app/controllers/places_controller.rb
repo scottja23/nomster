fc18bd41b4de24dc58fcfff5d805278413c49e79
Jackson Scott <scottja23@gmail.com>
2019-06-21 22:52:23 +0000

    added destroy place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,7 +2,7 @@ class PlacesController < ApplicationController
 before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    @places = Place.all
+    @places = Place.paginate(page: params[:page], per_page: 5)
   end
 
   def new
@@ -25,6 +25,12 @@ before_action :authenticate_user!, only: [:new, :create]
   def update
     @place = Place.find(params[:id])
     @place.update_attributes(place_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    @place = Place.find(params[:id])
+    @place.destroy
     redirect_to root_path
   end
 
