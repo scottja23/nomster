59882585e6e40c8370edda12586a3a0a1ba7dbf1
Jackson Scott <scottja23@gmail.com>
2019-06-30 19:57:16 +0000

    add photos of place

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,7 @@
 class Picture < ApplicationRecord
+  
   belongs_to :user
   belongs_to :place
   mount_uploader :pictures, PictureUploader
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_29_183713) do
+ActiveRecord::Schema.define(version: 2019_06_30_191039) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2019_06_29_183713) do
     t.datetime "updated_at", null: false
     t.string "pictures"
     t.integer "place_id"
+    t.string "photo"
   end
 
   create_table "places", force: :cascade do |t|
